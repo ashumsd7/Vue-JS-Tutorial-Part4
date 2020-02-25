bf52afe95a99de5eb2608c06c4a0ecdabe5d2ddb
ashu <aat8001@gmail.com>
2020-02-25 18:52:15 +0530

    Vue-Js Components
    props
    emit
    how parent- child communicate
    how child child communicate : eventBus

---
+++ src/components/User.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <hr>
+       <p>Name is in User Vue :<h4>{{name}}</h4></p>
+         <p>Age is in User Vue :<h4>{{age}}</h4></p>
+       
+
+
+
+        <button @click=changeName>Chnage My name</button>
+
+
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :name="name"
+                                @nameWasReset="name=$event"
+                                :resetFun="resetName"
+                                :userAge="age"
+                               
+                >
+
+
+                </app-user-detail>
+            </div>
+
+
+
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit
+                    :userAge="age"
+                     @ageWasEdited="age=$event"
+                ></app-user-edit>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+
+        data(){
+            return{
+                name : "ASHU",
+                age:27
+            }
+
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        },
+
+        methods:{
+            changeName(){
+
+                this.name= "AASIM";
+                
+            },
+
+            resetName(){
+                this.name= "ASHU"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+    <p>User Name: <h1>   {{name}}</h1> </p>
+     <p>User Age: <h1>   {{userAge}}</h1> </p>
+    <button @click="resetName">ResetName</button>
+
+    <button @click="resetFun()">ResetName</button>
+    </div>
+</template>
+
+<script>
+import  {eventBus} from '../main';
+
+export default{
+    props:['name','resetFun','userAge'],
+
+    //or use below
+    // props:{
+    //     name:{
+    //         type: String,
+    //     },
+    //     resetFun: Function
+    // }
+
+    methods:{
+        switchName(){
+            return this.name.split('').reverse().join('');
+        },
+        resetName(){
+            this.name= "ASHU";
+            this.$emit('nameWasReset',this.name)
+        }
+    },
+
+    created() {
+        eventBus.$on('ageWasEdited',(data)=>{
+            this.userAge= data;
+        });
+    },
+
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
